fa3669eb6671011826b9854b34fa76485d389ed8
ramanujdas <ramanujds9@gmail.com>
2020-10-16 14:36:50 +0530

    Added Constructor

--- capgemini-jee-workspace/basic-oop-concepts/src/com/cg/App.java
+++ capgemini-jee-workspace/basic-oop-concepts/src/com/cg/App.java
@@ -6,19 +6,16 @@ public class App {
 	public static void main(String[] args) {
 		
 		
-		Trainee t1=new Trainee();
-		Trainee t2=new Trainee();
+		Trainee t1=new Trainee("Priya Roy",23,"priya@gmail.com");
+		Trainee t2=new Trainee("Gaurav Sharma",28,"gaurav@gmail.com");
 		
-		t1.setAge(25);
+		
 		t1.setEmail("rahul@gmail.com");
 		t1.setTraineeName("Rahul Reddy");
-//		t1.showTrainee();
+		t1.showTrainee();
 		
-//		t2.age=23;
-//		t2.email="kavya@yahoo.com";
-//		t2.traineeName="Kavya Sharma";
-//		
-//		t2.showTrainee();
+
+		t2.showTrainee();
 		
 	
 		
--- capgemini-jee-workspace/basic-oop-concepts/src/com/cg/Trainee.java
+++ capgemini-jee-workspace/basic-oop-concepts/src/com/cg/Trainee.java
@@ -6,6 +6,20 @@ public class Trainee {
 	private int age;
 	private String email;
 	
+	public Trainee() {
+	}
+	
+	
+	
+	public Trainee(String traineeName, int age, String email) {
+	
+		this.traineeName = traineeName;
+		this.age = age;
+		this.email = email;
+	}
+
+
+
 	public String getTraineeName() {
 		return traineeName;
 	}
@@ -29,11 +43,11 @@ public class Trainee {
 	
 	
 	
-//	public void showTrainee() {
-//		System.out.println("Name = "+traineeName);
-//		System.out.println("Age = "+age);
-//		System.out.println("Email = "+email);
-//	}
+	public void showTrainee() {
+		System.out.println("Name = "+traineeName);
+		System.out.println("Age = "+age);
+		System.out.println("Email = "+email);
+	}
 	
 	
 	
