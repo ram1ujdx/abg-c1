60455b143da3f70b9069c204bdbe03f8d0b6f9d6
ramanujdas <ramanujds9@gmail.com>
2020-11-06 10:23:39 +0530

    Updated

--- capgemini-jee-workspace/first-web-project/src/com/cg/web/CubeServlet.java
+++ capgemini-jee-workspace/first-web-project/src/com/cg/web/CubeServlet.java
@@ -4,38 +4,47 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/add", initParams = {
-		@WebInitParam(name = "color", value = "red")
-})
-public class AdderServlet extends HttpServlet {
+/**
+ * Servlet implementation class CubeServlet
+ */
+@WebServlet("/cube")
+public class CubeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 		
-		int x,y,sum;
-		x=Integer.parseInt(request.getParameter("tnum1"));
-		y=Integer.parseInt(request.getParameter("tnum2"));
-		sum=x+y;
-		Cookie cookie=new Cookie("cg.web.sum", String.valueOf(sum));
-		//response.sendRedirect("cube?sum="+sum);
-		cookie.setMaxAge(60*60*24);	
-		response.addCookie(cookie);
+		
+		//int sum=Integer.parseInt(request.getParameter("sum"));
+		int sum=0;
+		Cookie []cookies=request.getCookies();
+		for(Cookie cookie:cookies) {
+			if(cookie.getName().equals("cg.web.sum")) {
+				sum=Integer.parseInt(cookie.getValue());
+				break;
+			}
+		}
+		
+		int cube=sum*sum*sum;
 		response.setContentType("text/html");
 		PrintWriter out=response.getWriter();
 		out.println("<html><head><title>Add Numbers</title></head>");
-		out.println("<body bgColor='yellow'>");
+		out.println("<body bgColor='skyblue'>");
 		out.println("<h1 style='color :blue;'>");
 				out.println("Sum = "+sum);
 				out.println("</h1>");
-//				out.println("<a href=cube?sum="+sum+">Get Cube</a>");
-				out.println("<a href=cube>Get Cube</a>");
+				out.println("<h1 style='color :green;'>");
+				out.println("Cube = "+cube);
+				out.println("</h1>");
 				out.println("</body>");
 				out.println("</html>");
 		
